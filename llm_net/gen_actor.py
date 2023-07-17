6a0688e6e226759bcef2c44dc2c65543a426bc0c
Cyrus Nouroozi <cyrus@edendaolab.com>
2023-07-17 14:52:11 -0600

    Fix GenAsyncAgent and add GenAsyncActor

--- llm_net/gen_actor.py
+++ llm_net/gen_actor.py
@@ -3,6 +3,7 @@ from typing import Any, Iterable
 import ray
 
 from llm_net.gen import GenAgent, Message
+from llm_net.gen_async import GenAsyncAgent
 
 
 @ray.remote(max_restarts=3, max_task_retries=3, num_cpus=1)
@@ -19,3 +20,17 @@ class GenActor:
     def property(self, prop: str) -> Any:
         """For getting arbitrary properties on the node"""
         return getattr(self.node, prop)
+
+
+@ray.remote(max_restarts=3, max_task_retries=3, num_cpus=1)
+class GenAsyncActor:
+    node: GenAsyncAgent
+
+    def __init__(self, node: GenAsyncAgent):
+        self.node = node
+
+    def property(self, prop: str) -> Any:
+        return getattr(self.node, prop)
+
+    async def areceive(self, message: Message):
+        return await self.node.areceive(message)
--- llm_net/gen_async.py
+++ llm_net/gen_async.py
@@ -1,7 +1,9 @@
+from typing import AsyncIterable, Optional, Union
+
 from pydantic import Field
 from pyee.asyncio import AsyncIOEventEmitter
 
-from llm_net.gen import GenAgent, GenAgentNet
+from llm_net.gen import GenAgent
 from llm_net.message import Message
 
 
@@ -10,28 +12,14 @@ class GenAsyncAgent(GenAgent):
         default_factory=AsyncIOEventEmitter, init=False
     )
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.event_emitter = AsyncIOEventEmitter()
+    async def areceive(
+        self, message: Optional[Message] = None
+    ) -> Union[None, Message, AsyncIterable[Message]]:
+        if message is None or message.sender == self or message.receiver != self:
+            return None
 
-    async def areceive(self, message: Message):
-        if message.sender == self.id:
-            return
+        self.emit("receive", message)
 
-        method = getattr(self, message.type, None)
-        if not method:
-            raise AttributeError(
-                f"{self.__class__.__name__} does not have a method named {message.type}"
-            )
-
-        async for self_response in method(message):
-            if (yield self_response) == StopAsyncIteration:
-                break
-            for node in self.successors(self):
-                async for link_response in node.areceive(self_response):
-                    if (yield link_response) == StopAsyncIteration:
-                        break
-
-
-class GenAsyncAgentNet(GenAgentNet):
-    pass
+        method = getattr(self, message.type) if message.type else self.call
+        return method(message)
+        return method(message)
