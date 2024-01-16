ab0cd40186860d9d79a22c993025f0920973e817
Cyrus Nouroozi <cyrus@edendaolab.com>
2024-01-15 21:52:12 -0600

    Nested CrewAI

--- tests/test_9_crewai.py
+++ tests/test_9_crewai.py
@@ -1,6 +1,7 @@
 import typing as t
 
-from beartype.typing import Optional
+from beartype.typing import Iterator, Optional
+from matchref import ref
 from pydantic import Field, computed_field
 from sorcery import maybe
 
@@ -28,7 +29,7 @@ class Agent(llegos.Actor):
     role: str
     goal: str
     backstory: str
-    llm: t.Any
+    executor: t.Any = Field(description="AgentExecutor")
     counter: int = Field(default=0)
 
     def receive_task(self, task: Task) -> str:
@@ -39,7 +40,7 @@ class Agent(llegos.Actor):
 class Crew(llegos.Network):
     tasks: list[Task]
 
-    def perform(self):
+    def perform(self) -> Iterator[Result]:
         result = None
         for task in self.tasks:
             agent = next(a for a in self.actors if a.can_receive(task))
@@ -54,30 +55,41 @@ class Crew(llegos.Network):
             )
             yield result
 
+    def receive_task(self, task: Task) -> Iterator[Result]:
+        # Plan some tasks
+        self.tasks = [task]
+        for result in self.perform():
+            yield result.forward_to(task.sender)
+
 
 def test_crewai():
     crew = Crew(
         actors=[
+            Crew(
+                actors=[
+                    Agent(
+                        name="Copywriter",
+                        role="copywriter",
+                        goal="",
+                        backstory="",
+                        executor=None,
+                    ),
+                ],
+                tasks=[],
+            ),
             Agent(
                 name="Copywriter",
                 role="copywriter",
                 goal="",
                 backstory="",
-                llm="",
+                executor=None,
             ),
             Agent(
                 name="Brand Strategist",
                 role="brand-strategist",
                 goal="",
                 backstory="",
-                llm="",
-            ),
-            Agent(
-                name="Social Media Marketer",
-                role="social-media-marketer",
-                goal="",
-                backstory="",
-                llm="",
+                executor=None,
             ),
         ],
         tasks=[
@@ -93,33 +105,18 @@ def test_crewai():
         ],
     )
 
+    nested_crew = crew.actors[0]
+    assert isinstance(nested_crew, Crew)
+    assert nested_crew.tasks == []
+
     # We can use the generator to keep running tasks until they are all done
-    generator = crew.perform()
+    for result in crew.perform():
+        assert result.parent, "Result should have a parent"
 
-    res_1 = t.cast(Result, next(generator))  # for type checking
-    assert res_1.content == "42!"
-    assert res_1.sender == crew.actors[0]
-    assert res_1.parent
-    assert res_1.parent.description == crew.tasks[0].description
-    assert res_1.parent.content == "Task 1"
-
-    res_2 = t.cast(Result, next(generator))  # for type checking
-    assert res_2.content == "42!!"
-    assert res_2.sender == crew.actors[0]
-    assert res_2.parent
-    assert res_2.parent.description == crew.tasks[1].description
-    assert res_2.parent.content == "42!\n\nTask 2"
-
-    res_3 = t.cast(Result, next(generator))  # for type checking
-    assert res_3.content == "42!!!"
-    assert res_3.sender == crew.actors[0]
-    assert res_3.parent
-    assert res_3.parent.description == crew.tasks[2].description
-    assert res_3.parent.content == "42!!\n\nTask 3"
-
-    try:
-        "Now we're done processing the tasks"
-        next(generator)
-        assert False
-    except StopIteration:
-        assert True
+        match result:
+            case Result(sender=ref.nested_crew, content="42!"):
+                assert result.parent.description == crew.tasks[0].description
+            case Result(sender=ref.nested_crew, content="42!!"):
+                assert result.parent.description == crew.tasks[1].description
+            case Result(sender=ref.nested_crew, content="42!!!"):
+                assert result.parent.description == crew.tasks[2].description
